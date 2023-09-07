054c12d888c2fe37ae4d72d6d45fc463199402e5
Gx-a <62352906+Gx-a@users.noreply.github.com>
2023-09-06 22:09:44 -0500

    a million chages and things added

---
+++ georgia_golding_working/js/toggle.js
@@ -0,0 +1,52 @@
+function toggleOne()
+           {
+            let div1 = document.getElementById('one');
+            
+            if(div1.innerHTML=='One')
+            {
+                div1.innerHTML='1';
+            }
+            else
+            {
+                div1.innerHTML='One';
+            }
+           }
+function toggleTwo()
+           {
+            let div2 = document.getElementById('two');
+            
+            if(div2.innerHTML=='Two')
+            {
+                div2.innerHTML='2';
+            }
+            else
+            {
+                div2.innerHTML='Two';
+            }
+           }           
+function toggleFour()
+           {
+            let div4 = document.getElementById('four');
+            
+            if(div4.innerHTML=='Four')
+            {
+                div4.innerHTML='4';
+            }
+            else
+            {
+                div4.innerHTML='Four';
+            }
+           }
+function toggleFive()
+           {
+            let div5 = document.getElementById('five');
+            
+            if(div5.innerHTML=='Five')
+            {
+                div5.innerHTML='5';
+            }
+            else
+            {
+                div5.innerHTML='Five';
+            }
+           }
