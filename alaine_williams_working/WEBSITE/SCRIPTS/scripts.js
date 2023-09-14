f031569e3c1327e6b885a05028e137a1129a5d84
alaine <williams.alaine241988@gmail.com>
2023-09-14 09:05:37 -0500

    chamges

---
+++ alaine_williams_working/WEBSITE/SCRIPTS/scripts.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mynav_bar").style.width = "250px";
+    }
+  
+  function closeNav() {
+    document.getElementById("mynav_bar").style.width = "0";
+  }
+  
